f434f2989ce1ebdcfaba16f191b0333335850417
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2021-12-08 10:06:55 +0330

    Update like.jsx

--- src/common/like.jsx
+++ src/common/like.jsx
@@ -1,1 +1,18 @@
+import React, { Component } from "react";
 
+class Like extends React.Component {
+  render() {
+    let classFont = "fa fa-heart";
+    if (!this.props.like) classFont += "-o";
+    return (
+      <i
+        onClick={this.props.onLike}
+        className={classFont}
+        style={{ cursor: "pointer" }}
+        aria-hidden="true"
+      ></i>
+    );
+  }
+}
+
+export default Like;
