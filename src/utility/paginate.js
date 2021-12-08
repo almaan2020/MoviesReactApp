62f78e2e0ac5f5b067aaf0ce98380fc32ff7e3af
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2021-12-09 02:13:34 +0330

    Update paginate.js

--- src/utility/paginate.js
+++ src/utility/paginate.js
@@ -1,1 +1,9 @@
+import _ from 'lodash';
 
+export function paginate(items, pageNumber, pageSize) {
+    const startIndex = (pageNumber - 1) * pageSize;
+    return _(items)     //return a lodash object
+        .slice(startIndex)   //slice lodash array from this index
+        .take(pageSize)    //take array to this index
+        .value();    //return a regular array
+}
