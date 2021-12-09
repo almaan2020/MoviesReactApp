49ea225d68c60df16a23ac0383543ba2a56a3d77
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2021-12-10 01:12:49 +0330

    Add files via upload

---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Like from "../common/like";
+import Table from "../common/table";
+
+class MoviesTable extends React.Component {
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    {
+      key: "like",
+      content: (movie) => (
+        <Like onLike={() => this.props.onLike(movie)} like={movie.like} />
+      ),
+    },
+    {
+      key: "delete",
+      content: (movie) => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      ),
+    },
+  ];
+
+  render() {
+    const { movies, onSort, sortColumn } = this.props;
+    return (
+      <Table
+        columns={this.columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+        data={movies}
+      />
+    );
+  }
+}
+
+export default MoviesTable;
