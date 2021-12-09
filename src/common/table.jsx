07e5b2e245554775f211f939ab3e68a90125c6d7
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2021-12-10 01:15:42 +0330

    Add files via upload

---
+++ src/common/table.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TableBody from "./tableBody";
+import TableHeader from "./tableHeader";
+
+const Table = (props) => {
+  const { columns, sortColumn, onSort, data } = props;
+  return (
+    <table className="table">
+      <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
+      <TableBody data={data} columns={columns} />
+    </table>
+  );
+};
+
+export default Table;
