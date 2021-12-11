45d607f50fad18e648c636b93cd357107d8331f8
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2021-12-12 01:27:29 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+// import Counter from './components/counter';
 import Movies from './components/movies';
+import Navbar from './components/navbar';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import Posts from './components/posts';
+import movieFrom from './components/movieFrom';
+import NotFound from './components/notFound';
 import './App.css';
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Navbar />
+      <div className="row">&nbsp;</div>
+
+      <Switch>
+        <Route path="/movies/:id" component={movieFrom} />
+        <Route path="/customers" render={(props) => <Customers title='Customers' {...props} />} />
+        <Route path="/rentals" component={Rentals} />
+        <Redirect from="/renta" to="rentals" />
+        <Route path="/posts/:year?/:month?" component={Posts} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/not-found" component={NotFound} />
+        <Route path="/" exact component={Movies} />
+        <Redirect to="/not-found" />
+      </Switch>
+
     </main>
   );
 }
