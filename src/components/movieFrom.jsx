3330d24caae6a8f9e74e53ce9441f86681ff418d
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2021-12-19 12:41:31 +0330

    Update movieFrom.jsx

--- src/components/movieFrom.jsx
+++ src/components/movieFrom.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import joi from "joi-browser";
 import Form from "../common/form";
-import { getMovie, saveMovie } from "../services/fakeMovieService.js";
-import { getGenres } from "../services/fakeGenreService.js";
+import { getMovie, saveMovie } from "../services/movieService.js";
+import { getGenres } from "../services/genreService.js";
 
 class MovieForm extends Form {
   state = {
@@ -30,18 +30,45 @@ class MovieForm extends Form {
     dailyRentalRate: joi.number().min(0).max(10).required().label("Rate"),
   };
 
-  componentDidMount() {
-    const genres = getGenres();
+  async componentDidMount() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
 
     const movieId = this.props.match.params.id;
     if (movieId === "new") return;
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found"); //return used for ingoring last line
-
-    this.setState({ data: this.mapToViewModel(movie) });
+    try {
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        this.props.history.replace("/not-found");
+    }
   }
+
+  //****refactoring componentDidMount****
+  // async populateGenres() {
+  //   const { data: genres } = await getGenres();
+  //   this.setState({ genres });
+  // }
+
+  // async populateMovie() {
+  //   try {
+  //     const movieId = this.props.match.params.id;
+  //     if (movieId === "new") return;
+
+  //     const { data: movie } = await getMovie(movieId);
+  //     this.setState({ data: this.mapToViewModel(movie) });
+  //   } catch (ex) {
+  //     if (ex.response && ex.response.status === 404)
+  //       this.props.history.replace("/not-found");
+  //   }
+  // }
+
+  // async componentDidMount() {
+  //   await this.populateGenres();
+  //   await this.populateMovie();
+  // }
 
   mapToViewModel(movie) {
     return {
@@ -53,10 +80,33 @@ class MovieForm extends Form {
     };
   }
 
-  dosubmit = () => {
-    saveMovie(this.state.data);
+  dosubmit = async () => {
+    await saveMovie(this.state.data);
     this.props.history.push("/movies");
   };
+
+  // handleAdd=async () =>{
+  //   const obj={title:'a',body:'b'};
+  //   const {data : post} = await axios.post(apiEndpoint,obj);
+
+  //   const posts=[post, ...this.state.posts];
+  //   setState({posts})
+  //   }
+  //   //browser =>network => response{data,status}
+
+  //   handleUpdate=async post=>{
+  //     post.title="UPDATED";
+  //   await axios.put(apiEndpoint+"/"+post.id , post);
+  //   //axios.patch(apiEndpoint+"/"+post.id , {title : post.title});
+
+  //    const posts = [...this.state.posts];
+
+  //      const index = posts.indexOf(post);
+
+  //      posts[index] = { ...post };
+
+  //    this.setState({ posts });
+  //   }
 
   render() {
     const { match } = this.props;
